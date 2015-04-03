e5ed9b94d6fbf8bd05fc2b998b1b539ddf7192a0
Dmitry Ahnazarov <dmitry.ahnazarov@o-labs.ru>
2015-04-04 02:47:54 +0600

    debug mail template

--- include/forms/forms_templates/mail_templates/client_mail.php
+++ include/forms/forms_templates/mail_templates/client_mail.php
@@ -1,13 +1,13 @@
 <p><b>Добрый день</b>,</p>
 <p>Мы получили Вашу заявку на регистрацию на 11 Ежегодный ECR Форум.<br>
 Заявка будет обработана в течение пяти рабочих дней.<br>
-Если Вы — платный участник, Вам будет выставлен счет, или отправлено подтверждение регистрации — если бесплатный. <br>
+Если Вы &mdash; платный участник, Вам будет выставлен счет, или отправлено подтверждение регистрации &mdash; если бесплатный. <br>
 Накануне мероприятия Вы получите СМС с уникальным четырехзначным кодом, который необходимо назвать на стойке регистрации вместе со своей фамилией, чтобы получить бейдж.<br>
-С уважением, Команда ECR  +7 (495) 735-43-78  <a href="mailto: office.ecr@gmail.com"> office.ecr@gmail.com</a></p>
+С уважением, Команда ECR  +7 (495) 735&mdash;43&mdash;s78  <a href="mailto: office.ecr@gmail.com"> office.ecr@gmail.com</a></p>
 <br>
 <p><b>Dear sender</b>,</p>
 <p>We've received your request for registration for the 11th Annual ECR Forum.<br>
 The request will be processed during the next 5 working days.<br>
-If you are a paying delegate you will receive a bill, if not — a confirmation of your registration for the event.<br>
+If you are a paying delegate you will receive a bill, if not &mdash; a confirmation of your registration for the event.<br>
 Before the event you will receive an SMS with a unique four-digit code to be called at the reception along with his name to get the badge.<br>
-Best regards, ECR-Rus Team +7 (495) 735-43-78 <a href="mailto: office.ecr@gmail.com"> office.ecr@gmail.com</a></p>
+Best regards, ECR&mdash;Rus Team +7 (495) 735&mdash;43&mdash;78 <a href="mailto: office.ecr@gmail.com"> office.ecr@gmail.com</a></p>
