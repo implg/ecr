f677409122e26f7ba05a092ae7ab37ecf5b8b3a5
Dmitry Ahnazarov <dmitry.ahnazarov@o-labs.ru>
2015-04-03 19:07:06 +0600

    Form with English

---
+++ include/forms/forms_templates/reg_form_en.php
@@ -0,0 +1,76 @@
+      
+      <form action="#action_url#" method="post" class="on_ajax">
+      
+        	#hiddens#
+         
+      <div class="form"> 
+      	<div class="price nopadding">
+        	<span class="dopinfo">
+		        <div>
+		          <p class="amount">#company_in_forum#</p>
+		          <label><h4>#company_in_forum_title#</h4></label>
+		        </div>
+	        </span>
+        </div>
+          #company#
+          #member#
+          #site_company#
+          #last_name#
+          #first_name#
+          #second_name#
+          #position#
+          #email#
+          #phone#
+          #cupon#
+          #count#
+          <h3>Bill-to information</h3>
+          #bik#
+          #bankname#
+          #ks#
+          #rs#
+          #urname#
+
+      
+		</div>
+      <div class="price">
+<h3>I’m going to attend the following sessions</h3>
+		#sess_iter#
+        <div>
+          <p class="amount">#sess#</p>
+          <label><h4>#sess_title#</h4></label>
+        </div>
+        #sess_enditer#
+
+
+       
+
+
+ <span class="dopinfo">
+
+ <h3>Do you need Russian-English translation during the Forum?</h3>
+		#translate_iter#
+        <div>
+          <p class="amount">#translate#</p>
+          <label><h4>#translate_title#</h4></label>
+        </div>
+        #translate_enditer#
+
+ <h3>Other information:</h3>
+ 		#who_iter#
+        <div>
+          <p class="amount">#who#</p>
+          <label><h4>#who_title#</h4></label>
+        </div>
+        #who_enditer#
+           
+  </span>      
+
+      </div>
+      <!-- prices ends! -->
+
+    </div>
+    
+<div class="butsub">
+ <button name="submit" type="submit" class="button">Registr</button>
+</div>
+        </form>
---
+++ include/ww.form.registr.en.php
@@ -0,0 +1,101 @@
+<?php
+error_reporting(E_ERROR); 
+ini_set("display_errors", 1); 
+				require_once($_SERVER["DOCUMENT_ROOT"] . "/include/forms/forms.php"); 
+				require_once($_SERVER["DOCUMENT_ROOT"] . "/include/companies.php"); 
+				$form = new Forms();
+				$params_form =  array(
+					'form_id' => 'registr_form_',
+					'form_title' => 'Разместить свою компанию',
+					'template' => 'registr_form',
+					'post_submit_message' => '<p class="align_center">Мы получили Вашу заявку на регистрацию на 11 Ежегодный ECR Форум.<br>
+Заявка будет обработана в течение пяти рабочих дней</p>',
+					'fields' => array(
+						'translate' => array('type' => 'radio',  'value' => 0, 'values' => 
+							array(
+								1 => array('value' => 1, 'title' => 'Yes'),
+								0 => array('value' => 0, 'title' => 'No'),
+								),
+							),
+
+						'sess' => array('type' => 'checkbox', 'required' => false, 'values' => 
+							array(
+								1 => array('value' => 1, 'title' => '<span>ECR</span> Plenary Session'),
+								2 => array('value' => 2, 'title' => '<span>Supply Chain</span> best practices'),
+								3 => array('value' => 3, 'title' => 'Shrinkage Reduction'),
+								4 => array('value' => 4, 'title' => 'On Shelf Availabillity'),
+								5 => array('value' => 5, 'title' => 'Category Management'),
+								6 => array('value' => 6, 'title' => 'Shopper Insights'),
+								7 => array('value' => 7, 'title' => '<span>Omni Channel Marketing</span>'),
+								8 => array('value' => 8, 'title' => 'Electronic Data Interchange (<span>EDI</span>)'),
+								9 => array('value' => 9, 'title' => 'E-invoices'),
+								10 => array('value' => 10, 'title' => 'Source e-documents'),
+								11 => array('value' => 11, 'title' => 'Master Data Synchronisation'),
+								11 => array('value' => 12, 'title' => 'ECR Best practices'),
+								),
+
+							),
+						
+						'company_in_forum' => array('type' => 'checkbox', 'title' => 'Is you company a member of ECR Russia?',  'required' => false),
+						'member' => array('type' => 'select', 'title' => 'Select company', 'values' => $companies, 'attrs' => array('style' => 'display:none;'),),
+						'language' => array('type' => 'hidden', 'value' => 'English'),
+						'company' => array('type' => 'text', 'required' => false, 'attrs'=>array('placeholder' => 'Company', 'class' => 'latine') ),
+						'site_company' => array('type' => 'text', 'required' => false,  'attrs'=>array('placeholder' => 'Company website') ),
+						'last_name' => array('extends' => 'name', 'attrs'=>array('placeholder' => 'Last name', 'class' => 'latine') ),
+						'first_name' => array('extends' => 'name', 'attrs'=>array('placeholder' => 'First name', 'class' => 'latine')),
+						
+						'position' => array('extends' => 'name', 'attrs'=>array('placeholder' => 'Position', 'class' => 'latine')),
+						'phone' => array('extends' => 'phone', 'attrs'=>array('placeholder' => 'Mobile phone number')),
+						'email' => array('extends' => 'email', 'attrs'=>array('placeholder' => 'E-mail address')),
+						'cupon' => array('extends' => 'name', 'required' => false,'attrs'=>array('placeholder' => 'Coupon (fill in to get the discount)')),
+						'count' => array('extends' => 'name', 'attrs' => array('placeholder' => 'How many delegates from your company are going to take part in the Forum?')),
+						'bik' => array('extends' => 'name', 'attrs' => array('placeholder' => 'IBAN code')),
+						'bankname' => array('extends' => 'name', 'attrs' => array('placeholder' => 'Bank name')),
+						'ks' => array('extends' => 'name', 'attrs' => array('placeholder' => 'SWIFT No')),
+						'rs' => array('extends' => 'name', 'attrs' => array('placeholder' => 'Account No')),
+						'urname' => array('extends' => 'name', 'attrs' => array('placeholder' => 'Organisation full name')),
+
+
+						'who' => array('type' => 'checkbox', 'required' => false, 'values' => 
+							array(
+								'1' => array('value' => 1, 'title' => 'I am a speaker at the Forum'),
+								'2' => array('value' => 2, 'title' => 'My company is a media partner of the Forum'),
+								'3' => array('value' => 3, 'title' => 'My company is a sponsor of the Forum'),
+								'4' => array('value' => 4, 'title' => 'My company is taking part in the Market-place of the Forum'),
+								
+								),
+
+							),
+						
+
+
+		
+					),
+					
+			
+					'email' => array(
+							'extends' => true,
+							'to' => array('wikedwolf@yandex.ru'), // or string or fieldKey 
+							'from' => 'no-reply@'.$_SERVER['SERVER_NAME'],
+							//'message' => 'text %NAME%',
+							'message_template' => 'admin_mail',
+							'subject' => 'ECR ФОРУМ 2015 - Регистрация',
+						),
+					'email_client' => array(
+							'extends' => true,
+							'to' => 'email', // or string or fieldKey 
+							'from' => 'no-reply@'.$_SERVER['SERVER_NAME'],
+							//'message' => 'text %NAME%',
+							'message_template' => 'client_mail',
+							'subject' => 'ECR ФОРУМ 2015 - Вы заполнили форму регистрации',
+						),
+					// 'sms' => array(
+					// 		'to' => array('89236388381'), // or fieldKey 
+					// 		'source' => 'site.ru',
+					// 		'message' => 'text %NAME%',
+					// 	),
+			 		);	
+			 		
+		
+				 echo $form->Init($params_form);
+		
