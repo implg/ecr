68bceb0f05f76798bd8400d221ee5beeee1d9484
Dmitry Ahnazarov <dmitry.ahnazarov@o-labs.ru>
2015-04-06 18:42:59 +0600

    Admin

--- admin/blocks/footer.php
+++ admin/blocks/footer.php
@@ -11,7 +11,7 @@
 	<!--  start footer-left -->
 	<div id="footer-left">
 	
-	Admin Skin &copy; Copyright Internet Dreams Ltd. <span id="spanYear"></span> <a href="">www.netdreams.co.uk</a>. All rights reserved.</div>
+	
 	<!--  end footer-left -->
 	<div class="clear">&nbsp;</div>
 </div>
