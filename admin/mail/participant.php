45da5cda2446ea98a9b8e5aec0c23871d579b57f
Dmitry Ahnazarov <dmitry.ahnazarov@o-labs.ru>
2015-04-06 20:40:51 +0600

    Debug messages and send change status message

---
+++ admin/mail/participant.php
@@ -0,0 +1,11 @@
+<p><b>Добрый день</b>,</p>
+<p>Вы прошли регистрацию на 11 Ежегодный ECR Форум.</p>
+<p>Накануне мероприятия Вы получите СМС с уникальным четырехзначным кодом, который необходимо назвать на стойке регистрации вместе со своей фамилией, чтобы получить бейдж.</p>
+<p>С уважением, Команда ECR +7 (495) 735-43-78 <a href="mailto:office.ecr@gmail.com">office.ecr@gmail.com</a></p>
+
+
+
+<p><b>Dear sender</b>,</p>
+<p>You are registered for the 11th Annual ECR Forum.</p>
+<p>Before the event you will receive an SMS with a unique four-digit code to be called at the reception along with his name to get the badge.</p>
+<p>Best regards, ECR-Rus Team +7 (495) 735-43-78  <a href="mailto:office.ecr@gmail.com">office.ecr@gmail.com</a></p>
--- admin/querysets/save.php
+++ admin/querysets/save.php
@@ -1,8 +1,15 @@
 <?php
 ini_set("display_errors", 1); 
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/forms/utils/mysql/query_builder.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/include/forms/utils/mail/phpmailer.php');
 $mysql = new simple_query_builder();
 $mysql->devMode = 1;
+$id = $_REQUEST['id'];
+$sql = "SELECT * 
+FROM  `members` WHERE id={$id};";
+
+$tickets = $mysql->select($sql);
+$ticket = $tickets[0];
 
 foreach ($_POST as $key => $value) {
 	if($key == 'sess' || $key == 'who'){
@@ -11,7 +18,46 @@ foreach ($_POST as $key => $value) {
 	$mysql->add($key, $value);
 
 }
+
+
+if ($ticket['status'] != 'Participant' && $_POST['status'] == 'Participant'){
+	$change_status = true;
+}else{
+	$change_status = false;
+}
 $res = $mysql->update('members', 'id='.$_REQUEST['id']);
+
+if($change_status){
+	
+	$email_template = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/admin/mail/participant.php');
+	$email_template = str_replace('«', '"', $email_template);
+	$email_template = str_replace('»', '"', $email_template);
+	$email_template = mb_convert_encoding($email_template, 'KOI8-R', 'UTF-8');
+	
+
+
+	$subject = "Статус заявки изменен на Participant";
+
+
+	$mail = new PHPMailer(true);
+	$mail->Subject = encodeSubject($subject, 'utf-8');
+	$mail->AddAddress($_REQUEST['email']);
+	$mail->From = 'no-reply@'.$_SERVER['SERVER_NAME'];
+	$mail->FromName = isset($email['from_name']) ? mb_convert_encoding($email['from_name'], 'KOI8-R', 'UTF-8') : $_SERVER['SERVER_NAME'];
+	$mail->AddReplyTo($email['from'], $email['from_name']);
+	$mail->Body  = $email_template;
+	$mail->AltBody = strip_tags($email_template);
+	
+	
+
+
+
+	$mail->Send(); 
+	$mail->ClearAttachments();
+	
+}
+
+
 
 if($res){
 	$message =  "Заявка успешно изменена";
@@ -20,3 +66,10 @@ if($res){
 	$message =  "Заявка не изменена";
 	header('Location: /admin/edit.php?id='.$_REQUEST['id'].'&message='.$message);
 }
+
+
+
+
+
+			
+	
