0724020bddb6d724b59e439f935a34d923f23cc6
Dmitry Ahnazarov <dmitry.ahnazarov@o-labs.ru>
2015-04-06 21:27:02 +0600

    Admin debug

--- admin/querysets/list.php
+++ admin/querysets/list.php
@@ -7,7 +7,7 @@ $sql = "SELECT *
 FROM  `members` WHERE 1=1";
 
 if ($_REQUEST['search_company_in_forum']){
-	$sql .= " AND company_in_forum<>0";
+	$sql .= " AND company_in_forum<>''";
 }
 if ($_REQUEST['search_last_name']){
 	$sql .= " AND last_name LIKE '%".$_REQUEST['search_last_name']."%'";
