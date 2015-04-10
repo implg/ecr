bea2f2f38bfe4025430c7e9af00845dbdaa413fd
Dmitry Ahnazarov <dmitry.ahnazarov@o-labs.ru>
2015-04-10 18:27:08 +0600

    Debug 506 dop

--- admin/querysets/list.php
+++ admin/querysets/list.php
@@ -7,8 +7,12 @@ $sql = "SELECT *
 FROM  `members` WHERE 1=1";
 
 if ($_REQUEST['search_company_in_forum']){
-	$sql .= " AND company_in_forum > ''";
+	$sql .= " AND company_in_forum == '1'";
 }
+if ($_REQUEST['search_no_company_in_forum']){
+	$sql .= " AND company_in_forum <> '1'";
+}
+
 if ($_REQUEST['search_last_name']){
 	$sql .= " AND last_name LIKE '%".$_REQUEST['search_last_name']."%'";
 }
