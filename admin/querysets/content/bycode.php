bcb23d90b4f227ea7d8592f5f8eeb362d3577653
Dmitry Akhnazarov <dmitry.akhnazarov@o-labs.ru>
2015-04-29 13:53:00 +0400

    Page admin

--- admin/querysets/content/bycode.php
+++ admin/querysets/content/bycode.php
@@ -5,7 +5,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/include/forms/utils/mysql/query_builder
 $mysql = new simple_query_builder();
 $mysql->devMode = 1;
 $sql = "SELECT * 
-FROM  `pages` WHERE alias={$pagecode};";
+FROM  `pages` WHERE alias='{$pagecode}';";
 
 $tickets = $mysql->select($sql);
 $page = $tickets[0];
