78f98a15f8bb852c5c59b6766193e7067356f700
Dmitry Akhnazarov <dmitry.akhnazarov@o-labs.ru>
2015-04-29 21:32:34 +0400

    index speakers

--- admin/querysets/speakers/main.php
+++ admin/querysets/speakers/main.php
@@ -1,10 +1,10 @@
 <?php
-ini_set("display_errors", 1); 
+ini_set("display_errors", 0); 
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/forms/utils/mysql/query_builder.php');
 $mysql = new simple_query_builder();
 $mysql->devMode = 1;
 $sql = "SELECT * 
-FROM  `speakers` WHERE 1=1";
+FROM  `speakers` WHERE in_main > 0";
 
 
 
