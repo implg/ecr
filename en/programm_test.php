11952431aed76a1d9a19549ae75f10b889b502d2
Dmitry Akhnazarov <dmitry.akhnazarov@o-labs.ru>
2015-04-29 14:14:17 +0400

    events

--- en/programm_test.php
+++ en/programm_test.php
@@ -69,12 +69,10 @@ ini_set("display_errors", 1); ?>
     	<h2>DAY <?php echo $day; ?>. <?php echo date('d.m.Y', strtotime($day[0]['day'])); ?></h2>
 
       <p class="subtitle"></p>
-      <?php if ($day == 2):?>
-    		</div>
-    		<div class="programm-wrap">
-    	<?php elseif($day == 1): ?>
-    		<div class="programm-wrap">
-    	<?php endif;?>
+      <div class="programm-wrap">
+    		
+    		
+    	
       <?php foreach ($day as $item): ?>
       	
       
