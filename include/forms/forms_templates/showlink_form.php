46d3316a9be7e900613f306de7523f7894532c34
Igor <igor.koval@o-labs.ru>
2015-06-11 13:28:09 +0300

    pop form

--- include/forms/forms_templates/showlink_form.php
+++ include/forms/forms_templates/showlink_form.php
@@ -1,31 +1,21 @@
-﻿      <div style="display:none;" id="fancybox_form">
-      <h2 nobr>Получить пароль</h2>
-      <form  action="#action_url#" method="post"  class="on_ajax">
-
-        	#hiddens#
-         
-      <div class="form showlink"> 
-      
-          #name#
-          #surname#
-          #company#
-         
-          #site_company#
-          #position#
-          #email#
-          #phone#
-     
-
-      
-		</div>
-    
-
-
+﻿<div class="fancybox_form_bg" style="display:none;"></div>
+<div style="display:none;" id="fancybox_form">
+  <h2 nobr>Получить пароль</h2>
+  <form  action="#action_url#" method="post"  class="on_ajax">
+  	#hiddens#
+    <div class="form showlink"> 
+        #name#
+        #surname#
+        #company#
        
-
-
-<div class="butsub">
- <button name="submit" type="submit" class="button">Получить пароль</button>
-</div>
-        </form>
+        #site_company#
+        #position#
+        #email#
+        #phone#
+		</div>
+  
+    <div class="butsub">
+      <button name="submit" type="submit" class="button">Получить пароль</button>
+    </div>
+  </form>
 </div>
