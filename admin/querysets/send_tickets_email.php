676ceed8a5b87fe1581b23872d30a7c5d3c2fbe4
Dmitry Akhnazarov <dmitry.akhnazarov@o-labs.ru>
2015-06-11 14:55:57 +0300

    Admin for showlink

--- admin/querysets/send_tickets_email.php
+++ admin/querysets/send_tickets_email.php
@@ -1,6 +1,6 @@
 <?php
 
-	ini_set("display_errors", 0); 
+	ini_set("display_errors", 1); 
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/forms/utils/mysql/query_builder.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/forms/utils/mail/phpmailer.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/companies.php' );
